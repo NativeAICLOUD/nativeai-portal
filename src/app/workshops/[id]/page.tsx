b8958ae2e6e94917da08a81b27b05d922e62651f
Berat <beratsulimani@gmail.com>
2024-10-06 21:41:29 +0200

    update

--- src/app/workshops/[id]/page.tsx
+++ src/app/workshops/[id]/page.tsx
@@ -14,8 +14,7 @@ const workshopList = [
 ];
 type IWorkshopList = typeof workshopList[0];
 
-
-export const ListData = [
+const ListData = [
     {
         title: "Introduction to Microsoft Azure",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi eveniet incidunt doloribus delectus tempore alias consequatur placeat velit beatae consequuntur qui voluptas quo iste et, enim atque. Incidunt, quisquam! Similique!"
